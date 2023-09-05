975efef4a0d0ebf7043a36641804b271e3b5cb19
Tim <tim@brightsidedeveloper.com>
2023-09-04 20:08:43 -0500

    Prisma SQL

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,10 @@
+import { ModeToggle } from '@/components/ModeToggle'
+
 export default function page() {
   return (
-    <div className='text-3xl text-indigo-500 font-bold'>PROTECTED ROUTE</div>
+    <div className='text-3xl text-indigo-500 font-bold'>
+      PROTECTED ROUTE
+      <ModeToggle />
+    </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -19,11 +20,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en' suppressHydrationWarning>
-        <body className={font.className}>
+        <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
           <ThemeProvider
             attribute='class'
             defaultTheme='dark'
-            enableSystem={false}
+            enableSystem
             storageKey='squad-chat-theme'
           >
             {children}
